b89c7571aed8307f5d2eb6decce5ddc28f1dd10e
bodroghykristof <bodroghykristof@gmail.com>
2020-09-21 14:45:13 +0200

    Create class Person

---
+++ src/main/java/com/codecool/Person.java
@@ -0,0 +1,29 @@
+package com.codecool;
+
+public class Person {
+
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age < 0) throw new IllegalArgumentException("Age cannot be lower than 0");
+        this.age = age;
+    }
+}
